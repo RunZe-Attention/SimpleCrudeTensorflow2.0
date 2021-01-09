d0ff3abcb92a4e28b6850883738c765427c5730e
yangrunze <18645697217@163.com>
2021-01-09 13:45:09 +0800

    房价预测

--- code4.py
+++ code4.py
@@ -8,3 +8,23 @@ y = (y_raw - y_raw.min()) / (y_raw.max() - y_raw.min())
 print(X)
 print(y)
 
+import tensorflow as tf
+X = tf.constant(X)
+y = tf.constant(y)
+a = tf.Variable(initial_value=0.)
+b = tf.Variable(initial_value=0.)
+variables = [a,b]
+num_epoch = 10000
+optimizer = tf.keras.optimizers.SGD(learning_rate=1e-3)
+for e in range(num_epoch):
+    with tf.GradientTape() as tape:
+        y_pred = a * X + b
+        loss = 0.5 * tf.reduce_sum(tf.square(y_pred - y))
+
+    grads = tape.gradient(loss,variables)
+
+    optimizer.apply_gradients(grads_and_vars=zip(grads,variables))
+
+print(a.numpy())
+print(b.numpy())
+
